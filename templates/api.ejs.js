dc562a267d0ab5a937ea9b24052e3e2fd9c24f71
libre <bej.delahanse@gmail.com>
2017-09-05 21:42:27 +0200

    FIX - api was not exposed correctly by export default

--- templates/api.ejs.js
+++ templates/api.ejs.js
@@ -26,32 +26,32 @@ import apisauce from 'apisauce'
 const env = process.env.NODE_ENV
 const devUrl = '<%- devUrl %>'
 const prodUrl = '<%- prodUrl %>'
-const rootUrl = env === 'production' ? prodUrl : devUrl
+const baseURL = env === 'production' ? prodUrl : devUrl
 let defaultHeaders = {}
 
 // Will try to find out default headers in the same file
 try {defaultHeaders = require('../defaultHeaders')}catch(e){}
 
-export default {
-  create<%- Name %>Api: rootUrl => {
+const create<%- Name %>Api = () => {
 
-    const api = apisauce.create({
-      rootUrl,
-      timeout: 10000
-    })
+  const api = apisauce.create({
+    baseURL,
+    timeout: 10000
+  })
 
-    return {
-      <% for (let action of actions) {
-      var defaultHttpType = setDefaultHttpType(action)
-      %>
-      <%- action.name %>: (args, additionalHeaders, httpType='<%- defaultHttpType %>') => {
-        let headers = defaultHeaders
-        if(additionalHeaders && (typeof additionalHeaders === 'object')) {
-          headers = {...defaultHeaders, ...additionalHeaders}
-        }
+  return {
+    <% for (let action of actions) {
+    var defaultHttpType = setDefaultHttpType(action)
+    %>
+    <%- action.name %>: (args, additionalHeaders, httpType='<%- defaultHttpType %>') => {
+      let headers = defaultHeaders
+      if(additionalHeaders && (typeof additionalHeaders === 'object')) {
+        headers = {...defaultHeaders, ...additionalHeaders}
+      }
 
-        return api[httpType]('<%- action.name %>', args, headers)
-      },<% } %>
-    }
+      return api[httpType]('<%- action.name %>', args, headers)
+    },<% } %>
   }
 }
+
+export default create<%- Name %>Api()
